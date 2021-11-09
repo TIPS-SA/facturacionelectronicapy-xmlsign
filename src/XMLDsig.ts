ee2934773cae046998a243dd141593f6ef35ba0a
Marcos Jara <adrianjara@gmail.com>
2021-11-09 18:11:26 -0300

    Agregado find-java-home y utilizado en llamada

--- src/XMLDsig.ts
+++ src/XMLDsig.ts
@@ -4,6 +4,8 @@ import xml2js from 'xml2js';
 import sha256 from 'crypto-js/sha256';
 import Base64 from 'crypto-js/enc-base64';
 import token from './PKCS12';
+import findJavaHome from 'find-java-home';
+
 //import crypto from 'crypto';
 //const { subtle } = require('crypto').webcrypto;
 const { exec } = require("child_process");
@@ -92,36 +94,43 @@ class XMLDsig {
         return new Promise(async (resolve, reject) => {
             //console.log("A firmar", xml);
             //xml = await this.asignarFechaFirma(xml);
-
-            const java8Path = `"C:\\Program Files\\Java\\jdk1.8.0_221\\bin\\java"`;
             
-            const classPath = '-classpath ' + __dirname + ' SignXML';
-            const tmpXMLToSign = '' + __dirname + '/xml_sign_temp.xml';
+            findJavaHome({allowJre: true}, (err: any, java8Path: any) => {
+                if(err)return console.log(err);
+                console.log("resultado de java8Path", java8Path);
 
-            fs.writeFileSync(tmpXMLToSign, xml, {encoding: 'utf8'});
-
-            exec(`${java8Path} -Dfile.encoding=IBM850 ${classPath} ${tmpXMLToSign} ${this.file} ${this.passphase}`, {encoding: "UTF-8"}, (error: any, stdout: any, stderr: any) => {
-                if (error) {
-                    reject(error);
-                }
-                if (stderr) {
-                    reject(stderr);
-                }
-                
-                try {
-                    fs.unlinkSync(tmpXMLToSign);
-                    //file removed
-                } catch(err) {
-                    console.error(err);
-                }
-
-                //console.log(`signedXML: ${stdout}`);
-
-                //resolve(Buffer.from(`${stdout}`,'utf8').toString());
-                //fs.writeFileSync(tmpXMLToSign + ".result.xml", `${stdout}`, {encoding: 'utf8'});
-                //let resultXML = fs.readFileSync(tmpXMLToSign + ".result.xml", {encoding: 'utf8'});
-                resolve(`${stdout}`);
+                //Comentar esta linea al llevar a Produccion, por que aqui trae java 6
+                java8Path = `"C:\\Program Files\\Java\\jdk1.8.0_221\\bin\\java"`;
+            
+                const classPath = '-classpath ' + __dirname + ' SignXML';
+                const tmpXMLToSign = '' + __dirname + '/xml_sign_temp.xml';
+    
+                fs.writeFileSync(tmpXMLToSign, xml, {encoding: 'utf8'});
+    
+                exec(`${java8Path} -Dfile.encoding=IBM850 ${classPath} ${tmpXMLToSign} ${this.file} ${this.passphase}`, {encoding: "UTF-8"}, (error: any, stdout: any, stderr: any) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    if (stderr) {
+                        reject(stderr);
+                    }
+                    
+                    try {
+                        fs.unlinkSync(tmpXMLToSign);
+                        //file removed
+                    } catch(err) {
+                        console.error(err);
+                    }
+    
+                    //console.log(`signedXML: ${stdout}`);
+    
+                    //resolve(Buffer.from(`${stdout}`,'utf8').toString());
+                    //fs.writeFileSync(tmpXMLToSign + ".result.xml", `${stdout}`, {encoding: 'utf8'});
+                    //let resultXML = fs.readFileSync(tmpXMLToSign + ".result.xml", {encoding: 'utf8'});
+                    resolve(`${stdout}`);
+                });
             });
+
         });
     }
 
