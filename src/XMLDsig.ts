96b6533cbee176258464162a8e076d16ee7a26d7
Marcos Jara <marcosjara@yahoo.es>
2022-04-06 14:25:17 -0400

    random file generated

--- src/XMLDsig.ts
+++ src/XMLDsig.ts
@@ -107,7 +107,8 @@ class XMLDsig {
         }
 
         const classPath = "" + __dirname + "";
-        const tmpXMLToSign = "" + __dirname + "/xml_sign_temp.xml";
+        //const tmpXMLToSign = "" + __dirname + "/xml_sign_temp.xml";
+        const tmpXMLToSign = "" + __dirname + "/xml_sign_temp_" + Math.round(Math.random() * 999999) + ".xml";
 
         fs.writeFileSync(tmpXMLToSign, xml, { encoding: "utf8" });
 
